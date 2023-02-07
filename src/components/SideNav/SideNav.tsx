1f5accbccf273536cada21f8782e624b6d2bc850
Lucas Ribeiro <lucas2001.789@gmail.com>
2023-02-06 21:18:45 -0300

    preparando aplicacao para responsividade

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -3,35 +3,52 @@ import { icons } from '../../assets/icons/icons';
 import { SideNavItem } from '../../core/models/SideNavIcon';
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { debounceTime, fromEvent } from 'rxjs';
 
 class SideNav extends Component {
+     public width = window.innerWidth;
 
      private readonly icons: SideNavItem[] = [
           new SideNavItem('ACCOUNT', icons.account),
      ]
 
+     public componentDidMount(): void {
+          fromEvent(window, 'resize').pipe(debounceTime(500)).subscribe(() =>
+               this.setState(() =>
+                    this.width = window.innerWidth
+               )
+          )
+     }
+
      public render() {
           return (
-               <div className='sidenav-content'>
-                    <div className="item">
-                         <Link className='logo' to='/'>
-                              <div className='logo-image' />
-                         </Link>
-                    </div>
+               <>
+                    {this.width > 992 ?
+                         <div className='sidenav-content'>
+                              <div className="item">
+                                   <Link className='logo' to='/'>
+                                        <div className='logo-image' />
+                                   </Link>
+                              </div>
 
-                    <div className="navigations">
-                         {
-                              this.icons.map((item, i) => (
-                                   <div className="item" key={i}>
-                                        <div className="icon" style={{
-                                             WebkitMaskImage: `url(${item.icon})`
-                                        }} />
-                                        <Link to='/favoritos' className='link'>{item.getTranslated()}</Link>
-                                   </div>
-                              ))
-                         }
-                    </div>
-               </div>
+                              <div className="navigations">
+                                   {
+                                        this.icons.map((item, i) => (
+                                             <div className="item" key={i}>
+                                                  <div className="icon" style={{
+                                                       WebkitMaskImage: `url(${item.icon})`
+                                                  }} />
+                                                  <Link to='/favoritos' className='link'>{item.getTranslated()}</Link>
+                                             </div>
+                                        ))
+                                   }
+                              </div>
+                         </div> :
+                         <div className="bottom-bar">
+
+                         </div>
+                    }
+               </>
           )
      }
 }
